936ff41414c1fdc862c6e7b13d92350dc51bcc3a
Simon Samuel <salviensky@gmail.com>
2024-08-21 20:24:41 +0100

    feat: added CRUD for bots

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BotsModule } from './bots/bots.module';
 
 @Module({
-  imports: [],
+  imports: [BotsModule],
   controllers: [AppController],
   providers: [AppService],
 })
