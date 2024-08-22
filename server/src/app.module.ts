a69260c6fd5f60444d23c03e7f854e54ef7260b4
Simon Samuel <salviensky@gmail.com>
2024-08-22 16:52:32 +0100

    feat: added posts resource feat: added comments resource feat: added relations between comments, posts and autobots entities

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AutobotsModule } from './autobots/autobots.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { Autobot } from './autobots/entities/autobot.entity';
+import { Comment } from './comments/entities/comment.entity';
+import { Post } from './posts/entities/post.entity';
 
 @Module({
-  imports: [AutobotsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'TweetAI',
+      synchronize: true,
+      entities: [Autobot, Comment, Post],
+    }),
+    AutobotsModule,
+    PostsModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/autobots/entities/autobot.entity.ts
+++ server/src/autobots/entities/autobot.entity.ts
@@ -1,1 +1,14 @@
-export class Autobot {}
+import { Post } from 'src/posts/entities/post.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Autobot {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Post, (post) => post.autobot)
+  posts: Post[];
+}
