8b6235677e9ee1d084e6d8d0b70242d41f007446
Simon Samuel <salviensky@gmail.com>
2024-08-22 18:16:31 +0100

    chore: switched to npm

--- server/src/autobots/dto/update-autobot.dto.ts
+++ server/src/autobots/dto/update-autobot.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateAutobotDto } from './create-autobot.dto';
 
 export class UpdateAutobotDto extends PartialType(CreateAutobotDto) {}
